7ebccb584523fe2db83885ba41f122902053135b
lorenzo angeli <lorenzo.angeli@efestolab.uk>
2016-10-04 12:52:24 +0200

    reimplement geometry with alembic and mayabinary

--- resource/application_hook/assets/geometry/geometry_asset.py
+++ resource/application_hook/assets/geometry/geometry_asset.py
@@ -2,6 +2,9 @@ import ftrack_api
 
 import ftrack_connect_pipeline.asset
 
+import tempfile
+import maya.cmds as mc
+
 IDENTIFIER = 'geometry'
 
 
@@ -110,6 +113,89 @@ class PublishGeometry(ftrack_connect_pipeline.asset.PublishAsset):
         }
 
         return options.get(name, [])
+
+    def _process_mayabinary(self, options):
+        # generate temp file
+        temporaryPath = tempfile.mkstemp(suffix='.mb')[-1]
+
+        export_selected = options.get('export_selected', False)
+        # save maya file
+        mc.file(
+            temporaryPath,
+            op='v=0',
+            typ='mayaBinary',
+            preserveReferences=options.get('reference', False),
+            constructionHistory=options.get('keep_history', False),
+            channels=options.get('keep_channels', False),
+            constraints=options.get('keep_constraints', False),
+            expressions=options.get('keep_expressions', False),
+            shader=options.get('keep_shaders', False),
+            exportSelected=export_selected,
+            exportAll=not export_selected,
+            force=True
+        )
+
+        return temporaryPath
+
+    def _process_alembic(self, options):
+
+        # export alembic file
+        temporaryPath = tempfile.mkstemp(suffix='.abc')[-1]
+
+        if options.get('export_selected', False):
+            nodes = mc.ls(sl=True, long=True)
+            selectednodes = None
+        else:
+            selectednodes = mc.ls(sl=True, long=True)
+            nodes = mc.ls(type='transform', long=True)
+
+        objCommand = ''
+        for n in nodes:
+            objCommand = objCommand + '-root ' + n + ' '
+
+        alembicJobArgs = ''
+
+        if options.get('uv_write', False):
+            alembicJobArgs += '-uvWrite '
+
+        if options.get('world_space', False):
+            alembicJobArgs += '-worldSpace '
+
+        if options.get('write_visibility', False):
+            alembicJobArgs += '-writeVisibility '
+
+        if options.get('animation', False):
+            alembicJobArgs += '-frameRange {0} {1} -step {2} '.format(
+                options.get('start_frame', 0),
+                options.get('end_frame', 0),
+                options.get('sampling', 1)
+            )
+
+        mc.loadPlugin('AbcExport.so', qt=1)
+
+        alembicJobArgs += ' ' + objCommand + '-file ' + temporaryPath
+        mc.AbcExport(j=alembicJobArgs)
+
+        if selectednodes:
+            mc.select(selectednodes)
+
+        return temporaryPath
+
+    def publish(self, publish_data):
+        '''Publish or raise exception if not valid.'''
+        processors = {
+            'maya.binary': self._process_mayabinary,
+            'maya.alembic': self._process_alembic
+        }
+        results = {}
+        for exporter in publish_data['item_options']:
+            process = processors.get(exporter)
+            if process:
+                results[exporter] = process(
+                    publish_data['item_options'][exporter]
+                )
+
+        print results
 
 
 def register(session):
