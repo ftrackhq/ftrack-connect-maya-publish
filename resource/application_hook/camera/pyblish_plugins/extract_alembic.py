fbc3fc6915d9374f02bd8c95686479fba3f3d29d
Mattias Lagergren <mattias.lagergren@ftrack.com>
2016-10-14 11:45:02 +0200

    Removed check for the publish flag

--- resource/application_hook/camera/pyblish_plugins/extract_alembic.py
+++ resource/application_hook/camera/pyblish_plugins/extract_alembic.py
@@ -1,82 +1,85 @@
 import pyblish.api
 
 
-class ExtractAlembic(pyblish.api.InstancePlugin):
-    '''prepare component to be published'''
+class ExtractCameraAlembic(pyblish.api.InstancePlugin):
+    '''Prepare component to be published.'''
+
     order = pyblish.api.ExtractorOrder
+
     families = ['ftrack.maya.camera']
 
     def process(self, instance):
-        print 'PROCESSING ALEMBIC', instance.data.get('publish')
+        '''Process *instance*.'''
         import maya.cmds as mc
         import tempfile
 
         mc.select(str(instance), replace=True)
 
-        if instance.data.get('publish'):
-            context_options = instance.context.data['options'].get('alembic', {})
-            print (
-                'Extracting Alembic using options:',
-                context_options
+        context_options = instance.context.data['options'].get(
+            'alembic', {}
+        )
+        print (
+            'Extracting Alembic using options:',
+            context_options
+        )
+
+        # extract options
+        animation = context_options.get('animation', False)
+        uv_write = context_options.get('uv_write', True)
+        start_frame = context_options.get('start_frame', 0)
+        end_frame = context_options.get('end_frame', 1)
+        world_space = context_options.get('world_space', True)
+        write_visibility = context_options.get('write_visibility', True)
+        sampling = context_options.get('sampling', 0.1)
+        export_selected = context_options.get('export_selected', True)
+
+        # export alembic file
+        temporaryPath = tempfile.mkstemp(suffix='.abc')[-1]
+
+        if export_selected:
+            nodes = mc.ls(sl=True, long=True)
+            selectednodes = None
+        else:
+            selectednodes = mc.ls(sl=True, long=True)
+            nodes = mc.ls(type='transform', long=True)
+
+        objCommand = ''
+        for n in nodes:
+            objCommand = objCommand + '-root ' + n + ' '
+
+        alembicJobArgs = ''
+
+        if uv_write:
+            alembicJobArgs += '-uvWrite '
+
+        if world_space:
+            alembicJobArgs += '-worldSpace '
+
+        if write_visibility:
+            alembicJobArgs += '-writeVisibility '
+
+        if animation:
+            alembicJobArgs += '-frameRange {0} {1} -step {2} '.format(
+                start_frame,
+                end_frame,
+                sampling
             )
 
-            # extract options
-            animation = context_options.get('animation', False)
-            uv_write = context_options.get('uv_write', True)
-            start_frame = context_options.get('start_frame', 0)
-            end_frame = context_options.get('end_frame', 1)
-            world_space = context_options.get('world_space', True)
-            write_visibility = context_options.get('write_visibility', True)
-            sampling = context_options.get('sampling', 0.1)
-            export_selected = context_options.get('export_selected', True)
+        mc.loadPlugin('AbcExport.so', qt=1)
 
-            # export alembic file
-            temporaryPath = tempfile.mkstemp(suffix='.abc')[-1]
+        alembicJobArgs += ' ' + objCommand + '-file ' + temporaryPath
+        mc.AbcExport(j=alembicJobArgs)
 
-            if export_selected:
-                nodes = mc.ls(sl=True, long=True)
-                selectednodes = None
-            else:
-                selectednodes = mc.ls(sl=True, long=True)
-                nodes = mc.ls(type='transform', long=True)
+        if selectednodes:
+            mc.select(selectednodes)
 
-            objCommand = ''
-            for n in nodes:
-                objCommand = objCommand + '-root ' + n + ' '
+        new_component = {
+            'name': '%s.alembic' % instance.name,
+            'path': temporaryPath,
+        }
 
-            alembicJobArgs = ''
-
-            if uv_write:
-                alembicJobArgs += '-uvWrite '
-
-            if world_space:
-                alembicJobArgs += '-worldSpace '
-
-            if write_visibility:
-                alembicJobArgs += '-writeVisibility '
-
-            if animation:
-                alembicJobArgs += '-frameRange {0} {1} -step {2} '.format(
-                    start_frame,
-                    end_frame,
-                    sampling
-                )
-
-            mc.loadPlugin('AbcExport.so', qt=1)
-
-            alembicJobArgs += ' ' + objCommand + '-file ' + temporaryPath
-            mc.AbcExport(j=alembicJobArgs)
-
-            if selectednodes:
-                mc.select(selectednodes)
-
-            new_component = {
-                'name': '%s.alembic' % instance.name,
-                'path': temporaryPath,
-            }
-
-            print 'Adding new component: %s' % new_component
-            instance.data['ftrack_components'].append(new_component)
+        print 'Adding new component: %s' % new_component
+        instance.data['ftrack_components'].append(new_component)
 
 
-pyblish.api.register_plugin(ExtractAlembic)
+pyblish.api.register_plugin(ExtractCameraAlembic)
--- resource/application_hook/camera/pyblish_plugins/extract_mayabinary.py
+++ resource/application_hook/camera/pyblish_plugins/extract_mayabinary.py
@@ -16,47 +16,46 @@ class ExtractMayaBinary(pyblish.api.InstancePlugin):
         # select the given geometries
         mc.select(str(instance), replace=True)
 
-        if instance.data.get('publish'):
-            context_options = instance.context.data['options'].get('maya_binary', {})
-            print (
-                'Extracting MayaBinary using options:',
-                context_options
-            )
-            # extract options and provide defaults
-            keep_reference = context_options.get('reference', False)
-            keep_history = context_options.get('history', False)
-            keep_channels = context_options.get('channels', False)
-            keep_constraints = context_options.get('constraint', False)
-            keep_expressions = context_options.get('expression', False)
-            keep_shaders = context_options.get('shaders', True)
-            export_selected = context_options.get('export_selected', True)
+        context_options = instance.context.data['options'].get('maya_binary', {})
+        print (
+            'Extracting MayaBinary using options:',
+            context_options
+        )
+        # extract options and provide defaults
+        keep_reference = context_options.get('reference', False)
+        keep_history = context_options.get('history', False)
+        keep_channels = context_options.get('channels', False)
+        keep_constraints = context_options.get('constraint', False)
+        keep_expressions = context_options.get('expression', False)
+        keep_shaders = context_options.get('shaders', True)
+        export_selected = context_options.get('export_selected', True)
 
-            # generate temp file
-            temporaryPath = tempfile.mkstemp(suffix='.mb')[-1]
+        # generate temp file
+        temporaryPath = tempfile.mkstemp(suffix='.mb')[-1]
 
-            # save maya file
-            mc.file(
-                temporaryPath,
-                op='v=0',
-                typ='mayaBinary',
-                preserveReferences=keep_reference,
-                constructionHistory=keep_history,
-                channels=keep_channels,
-                constraints=keep_constraints,
-                expressions=keep_expressions,
-                shader=keep_shaders,
-                exportSelected=export_selected,
-                exportAll=not export_selected,
-                force=True
-            )
+        # save maya file
+        mc.file(
+            temporaryPath,
+            op='v=0',
+            typ='mayaBinary',
+            preserveReferences=keep_reference,
+            constructionHistory=keep_history,
+            channels=keep_channels,
+            constraints=keep_constraints,
+            expressions=keep_expressions,
+            shader=keep_shaders,
+            exportSelected=export_selected,
+            exportAll=not export_selected,
+            force=True
+        )
 
-            new_component = {
-                'name': '%s.mayabinary' % instance.name,
-                'path': temporaryPath,
-            }
+        new_component = {
+            'name': '%s.mayabinary' % instance.name,
+            'path': temporaryPath,
+        }
 
-            print 'Adding new component: %s' % new_component
-            instance.data['ftrack_components'].append(new_component)
+        print 'Adding new component: %s' % new_component
+        instance.data['ftrack_components'].append(new_component)
 
 
 pyblish.api.register_plugin(ExtractMayaBinary)
