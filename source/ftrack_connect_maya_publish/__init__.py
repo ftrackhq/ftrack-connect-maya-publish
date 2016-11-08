18ebf4414f8068c05a755ecb2a86a3acde4dc8a8
Mattias Lagergren <mattias.lagergren@ftrack.com>
2016-11-08 11:53:34 +0100

    Remove internal registration of assets

--- source/ftrack_connect_maya_publish/__init__.py
+++ source/ftrack_connect_maya_publish/__init__.py
@@ -1,45 +1,14 @@
 # :coding: utf-8
 # :copyright: Copyright (c) 2016 ftrack
 
-import os
-import functools
 
-import pyblish.plugin
 import ftrack_api.event.base
 
 from ._version import __version__
 
-from ftrack_connect_maya_publish.asset.camera import camera_asset
-from ftrack_connect_maya_publish.asset.geometry import geometry_asset
-from ftrack_connect_maya_publish.asset.scene import scene_asset
-
-
-def register_callback(session, event):
-    '''Handle register event.'''
-    camera_asset.register(session)
-    geometry_asset.register(session)
-    scene_asset.register(session)
-
-    path = os.path.normpath(
-        os.path.join(
-            os.path.abspath(
-                os.path.dirname(
-                    __file__
-                )
-            ),
-            'common_pyblish_plugins'
-        )
-    )
-    pyblish.plugin.register_plugin_path(path)
-
 
 def register_assets(session):
     '''Register by emitting event on *session*.'''
-    session.event_hub.subscribe(
-        'topic=ftrack.pipeline.register-assets',
-        functools.partial(register_callback, session)
-    )
-
     session.event_hub.publish(
         ftrack_api.event.base.Event(
             topic='ftrack.pipeline.register-assets',
